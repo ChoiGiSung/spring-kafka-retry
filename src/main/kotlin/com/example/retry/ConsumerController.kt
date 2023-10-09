8f2ff6c347792909f0dee1f5a8b4820a75708494
DESKTOP-MJ25L1Q\chlrl <gisungcu@gmail.com>
2023-10-09 13:41:09 +0900

    RetryableTopic sample

--- src/main/kotlin/com/example/retry/ConsumerController.kt
+++ src/main/kotlin/com/example/retry/ConsumerController.kt
@@ -1,14 +1,29 @@
 package com.example.retry
 
+import org.springframework.kafka.annotation.DltHandler
 import org.springframework.kafka.annotation.KafkaHandler
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.annotation.RetryableTopic
+import org.springframework.retry.annotation.Backoff
 import org.springframework.stereotype.Component
-
+import org.springframework.kafka.retrytopic.DltStrategy
+import org.springframework.kafka.retrytopic.TopicSuffixingStrategy
+import java.util.concurrent.TimeoutException
 @Component
 @KafkaListener(groupId = "sample-1", topics = ["testTopic"])
 class ConsumerController {
 
+    //todo test
     @KafkaHandler
+    @RetryableTopic(
+        backoff = Backoff(value = 3000L),
+        attempts = "4",
+        autoCreateTopics = "true", //default
+        topicSuffixingStrategy = TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE, // -0, -1 suffix
+        dltStrategy = DltStrategy.FAIL_ON_ERROR, // dlt 전송 전략
+        include = [TimeoutException::class], //리트라이함
+        exclude = [NullPointerException::class] //리트라이 안함
+    )
     fun consume(userInfo: UserInfo) {
         println(userInfo)
     }
@@ -17,6 +32,12 @@ class ConsumerController {
     fun consume(userInfo: OtherUserInfo) {
         println(userInfo)
     }
+
+    @DltHandler
+    fun handleDlt(event: Any) {
+        println("dlt event: $event")
+    }
+
 
     data class UserInfo(
         val name: String,
