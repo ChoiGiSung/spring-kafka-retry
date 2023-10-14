bf5c4a272db1b7f8790233b9c1e319539f2b7b05
DESKTOP-MJ25L1Q\chlrl <gisungcu@gmail.com>
2023-10-14 18:04:07 +0900

    add producer test

--- src/test/kotlin/com/example/retry/RetryApplicationTests.kt
+++ src/test/kotlin/com/example/retry/RetryApplicationTests.kt
@@ -1,13 +1,23 @@
 package com.example.retry
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class RetryApplicationTests {
 
+	@Autowired
+	lateinit var producer: CustomProducer
+
 	@Test
-	fun contextLoads() {
+	fun something() {
+		producer.send(CustomProducer.UserInfo("홍길동", "010-1234-5678", "홍홍"))
+	}
+
+	@Test
+	fun something2() {
+		producer.send(CustomProducer.OtherUserInfo("홍길동", 12))
 	}
 
 }
