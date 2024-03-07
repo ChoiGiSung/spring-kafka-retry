1e5d431948788b3bfc40d661d51c87682abafc63
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2024-03-08 00:30:21 +0900

    Change properties to Kotlin code

--- src/main/kotlin/com/example/retry/KafkaConfig.kt
+++ src/main/kotlin/com/example/retry/KafkaConfig.kt
@@ -1,19 +1,24 @@
 package com.example.retry
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.boot.autoconfigure.kafka.DefaultKafkaConsumerFactoryCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.retrytopic.RetryTopicConfiguration
 import org.springframework.kafka.retrytopic.RetryTopicConfigurationBuilder
 import org.springframework.kafka.retrytopic.TopicSuffixingStrategy
 import org.springframework.kafka.support.EndpointHandlerMethod
-
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer
+import org.springframework.kafka.support.serializer.JsonDeserializer
 
 
 @Configuration
 class KafkaConfig(
-) {
+    private val objectMapper: ObjectMapper
+) : DefaultKafkaConsumerFactoryCustomizer {
 
     @Bean
     fun retryTopicConfig(
@@ -29,8 +34,28 @@ class KafkaConfig(
             .dltSuffix("-coco-dlt")
             .listenerFactory(concurrentKafkaListenerContainerFactory)
             .setTopicSuffixingStrategy(TopicSuffixingStrategy.SUFFIX_WITH_INDEX_VALUE)
-            .dltHandlerMethod(EndpointHandlerMethod(ConsumerErrorsHandler::class.java, ConsumerErrorsHandler::postProcessDltMessage.name))
+            .dltHandlerMethod(
+                EndpointHandlerMethod(
+                    ConsumerErrorsHandler::class.java,
+                    ConsumerErrorsHandler::postProcessDltMessage.name
+                )
+            )
             .create(kafkaTemplate)
+    }
+
+    override fun customize(consumerFactory: DefaultKafkaConsumerFactory<*, *>) {
+        (consumerFactory as DefaultKafkaConsumerFactory<String, Any>).setValueDeserializer(
+            errorHandlingDeserializer()
+        )
+    }
+
+    private fun errorHandlingDeserializer(): ErrorHandlingDeserializer<Any> {
+        val errorHandlingDeserializer = ErrorHandlingDeserializer<Any>(
+            JsonDeserializer<Any>(
+                objectMapper
+            )
+        )
+        return errorHandlingDeserializer
     }
 
 }
